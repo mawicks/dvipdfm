73d3bfd35d7eab733ec478c19374591fa88fbbdd
mwicks <mwicks>
1998-12-11 23:07:43 +0000

    Adding acconfig.h

---
+++ dvipdfm/acconfig.h
@@ -0,0 +1,19 @@
+
+/* Local variables defined by MAW */
+
+#define VERSION 0
+
+/* HAVE_TIMEZONE - Define if <time.h> has timezone as an external variable */
+#undef HAVE_TIMEZONE
+
+/* HAVE_TM_GMTOFF - Define if struct tm has tm_gmtoff as a member */
+#undef HAVE_TM_GMTOFF
+
+/* KPATHSEA - Define if you have kpathsea */
+#undef KPATHSEA
+
+/* HAVE_KPSE_FORMATS - Define if you have a
+ version of kpathsea with afm, type1, and vf formats */
+#undef HAVE_KPSE_FORMATS
+
+
